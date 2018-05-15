e8cbf1cfdd1b7c84eaeeaf90eeeb767dfab697d1
Igor <verycooleagle@gmail.com>
2018-05-15 21:18:24 +0300

    Ready.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # fs-tree-view
 Get struct director
+
+Run example
+___
+```
+node example.js
+```
+
+Result
+___
+```
+Root folder
+├── LICENSE  -- "About license"
+├── TreeView.js  -- "Annotation class"
+└── example.js  -- "Example use"
+```
---
+++ example.js
@@ -0,0 +1,21 @@
+const TreeView = require('./TreeView');
+const treeView = new TreeView();
+
+void async function () {
+	await treeView.scan(__dirname);
+
+	let content = treeView.toContent([
+		{
+			regExp : /example\.js/,
+			text : "Example use"
+		},
+		{
+			regExp : /TreeView\.js/,
+			text : "Annotation class"
+		},
+		{
+			regExp : /LICENSE/,
+			text : "About license"
+		},
+	]);
+}();
